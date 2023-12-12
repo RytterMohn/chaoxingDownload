f51d4b6c8d7259e20bbf8667391cc959c729f685
Rytter <1198033108@qq.com>
2023-12-12 21:52:22 +0800

    Update Download.js
    1.11

--- Download.js
+++ Download.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         超星学习通章节内ppt视频下载
 // @namespace    http://tampermonkey.net/
-// @version      1.10
+// @version      1.11
 // @description  按下D下载ppt,pdf,上课视频
 // @author       西电网信院的废物rytter & 西电网信院的废物B4a 
 // @match        http://*.chaoxing.com/mycourse/*
@@ -19,7 +19,7 @@
 // @downloadURL
 // @license      MIT
 // ==/UserScript==
-
+ 
 function get_xueyinonline_objectid() {
     var ans_classes = document.getElementsByClassName("ans-attach-ct");
     // support old version used by learning.xidian.edu.cn
@@ -45,7 +45,7 @@ function get_xueyinonline_objectid() {
     console.log("任务对象：", objectids);
     return objectids;
 }
-
+ 
 function get_chaoxing_objectid() {
     var ans_classes = document
         .getElementsByTagName("iframe")[0]
@@ -69,17 +69,17 @@ function get_chaoxing_objectid() {
     console.log(objectids);
     return objectids;
 }
-
+ 
 function download(i) {
     // firstly, we need to get the objectid
     var tipsDiv = document.querySelector('.tips');
-
+ 
     if (!tipsDiv) {
         // If it doesn't exist, create it and set styles to make it float in the top right corner
         tipsDiv = document.createElement('div');
         tipsDiv.className = 'tips';
         document.body.appendChild(tipsDiv);
-
+ 
         // Apply styles directly or add a class
         tipsDiv.style.position = 'fixed';
         tipsDiv.style.top = '10px';
@@ -89,26 +89,26 @@ function download(i) {
         tipsDiv.style.zIndex = '1000'; // Ensure it's on top of other elements
         tipsDiv.style.textAlign = 'right'; // Align text to the right
     }
-
+ 
     // unhide
     if (tipsDiv.style.zIndex === '-1') {
         tipsDiv.style.zIndex = '1000';
     }
-
+ 
     tipsDiv.innerHTML = '<i></i>';
-
+ 
     var objectids = get_xueyinonline_objectid();
     if (objectids.length == 0) {
         objectids = get_chaoxing_objectid();
     }
-
+ 
     var objectid = objectids[i % objectids.length];
     console.log(objectid);
-
+ 
     var protocolStr = document.location.protocol;
     var url = protocolStr + "//mooc1." + (window.location.href.includes("xueyinonline") ? "xueyinonline" : "chaoxing") + ".com/ananas/status/" + objectid + "?flag=normal";
     console.log("资源URL：", url);
-
+ 
     // 使用fetch API获取文件信息
     fetch(url)
         .then(response => response.json())
@@ -128,48 +128,57 @@ function download(i) {
                 fileUrl = json.pdf || json.download;
                 fullFileName = json.filename;
             }
-
+ 
             // Create elements for file name, file size, and percentage
             var fileNameSpan = document.createElement('span');
             fileNameSpan.textContent = '下载文件： ' + fullFileName;
             fileNameSpan.style.marginRight = '10px'; // Add margin to the right
-
-
+ 
+ 
             var percentageSpan = document.createElement('span');
             percentageSpan.textContent = '0%';
             percentageSpan.style.marginLeft = '10px'; // Add margin to the right
             percentageSpan.style.width = '60px';
-
+ 
             // Create a progress bar element and append it to the tips div
             var progressBar = document.createElement('progress');
             var fileSizeSpan = document.createElement('span');
             fileSizeSpan.textContent = '文件大小：计算中……';
             fileSizeSpan.style.marginRight = '10px'; // Add margin to the right
-
+ 
             progressBar.value = 0;
             progressBar.max = 100;
             tipsDiv.appendChild(fileNameSpan);
             tipsDiv.appendChild(fileSizeSpan);
             tipsDiv.appendChild(progressBar);
             tipsDiv.appendChild(percentageSpan);
-
+ 
             var xhr = new XMLHttpRequest();
             xhr.open('GET', fileUrl, true);
             xhr.responseType = 'blob';
-
+ 
             xhr.onprogress = function(event) {
                 if (event.lengthComputable) {
                     var percentComplete = (event.loaded / event.total) * 100;
                     progressBar.value = percentComplete; // Update the progress bar value
                     percentageSpan.textContent = percentComplete.toFixed(2) + '%'; // Update the percentage text
                     fileSizeSpan.textContent = '文件大小：' + (event.total / 1024 / 1024).toFixed(2) + ' MB'; // Update the file size text
-
                 }
             };
-
+ 
             xhr.onload = function(event) {
                 if (this.status === 200) {
                     var blob = this.response;
+ 
+                    if (blob.type === 'application/pdf') {
+                        // Check if the blob type is application/pdf
+                        // Modify the filename to have the .pdf extension
+                        var extension = '.pdf';
+                        if (!fullFileName.endsWith(extension)) {
+                            // If the filename doesn't end with .pdf, modify it
+                            fullFileName = fullFileName.replace(/\.[^.]+$/, '') + extension;
+                        }
+                    }
                     var downloadUrl = window.URL.createObjectURL(blob);
                     var a = document.createElement("a");
                     a.href = downloadUrl;
@@ -184,29 +193,24 @@ function download(i) {
                     tipsDiv.style.zIndex = '-1';
                 }
             };
-
+ 
             xhr.onerror = function() {
                 console.error("Error fetching the file for download.");
                 tipsDiv.innerHTML = '下载失败！'; // Display error message in tips div
             };
-
+ 
             xhr.send();
-
+ 
         })
         .catch(error => {
             console.error("Error fetching the JSON data:", error);
         });
 }
-
-
+ 
+ 
 (function() {
     "use strict";
     var i = 0;
-    // redirect to http, to avoid download failure (optimised for learning.xidian.edu.cn)
-    var targetProtocol = "https:";
-    if (window.location.protocol != targetProtocol)
-        window.location.href = targetProtocol +
-        window.location.href.substring(window.location.protocol.length);
     document.onkeydown = function(e) {
         //对整个页面监听
         var keyNum = window.event ? e.keyCode : e.which; //获取被按下的键值
